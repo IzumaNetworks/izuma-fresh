0d239b9df497822dbe2bcff1c694820de71736af
edhemphill <edhemphill@gmail.com>
2022-11-16 13:53:11 -0600

    changes to help support support site

--- static/js/fresh.js
+++ static/js/fresh.js
@@ -52,6 +52,7 @@ $(document).ready(function(){
     if ($('#navbar-clone').length) {
         $(window).scroll(function() {    // this will work when your window scrolled.
             var height = $(window).scrollTop();  //getting the scrolling height of window
+            console.log("scroll: ",height);
             if(height  > 50) {
                 $("#navbar-clone").addClass('is-active');
             } else{
